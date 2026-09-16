0c8bfca414dd99b4bb53b2caf560e88f50a70877
moraeslima518-glitch <moraeslima518@gmail.com>
2026-09-16 18:56:52 -0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,1 +1,14 @@
+import telebot
+
+# Token novo do seu bot
+TOKEN = "8702525061:AAEG19ix4ksHGz4di46aaoSIcQOkI1pNl0U"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def enviar_boas_vindas(mensagem):
+    bot.reply_to(mensagem, "🤖 Olá! Bot do zero criado com sucesso e operando perfeitamente!")
+
+print("Bot iniciado com sucesso. Aguardando mensagens...")
+bot.infinity_polling()
 
